f42f730ebbcba7f5fed8c83c9687ec605eaf5166
IamMulanga <siphumamulanga@gmail.com>
2024-08-05 08:30:35 +0200

    Validating the vehicle license plate number and the selection of country field

--- libs/cross-border-letter/cypress.config.ts
+++ libs/cross-border-letter/cypress.config.ts
@@ -1,9 +1,10 @@
 import { nxComponentTestingPreset } from '@nx/angular/plugins/component-testing';
+import { nxComponentTestingPreset } from '@nx/angular/plugins/component-testing';
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
   component: {
     ...nxComponentTestingPreset(__filename),
-    indexHtmlFile: './cypress/support/component-index.html'
-  }
+    indexHtmlFile: './cypress/support/component-index.html',
+  },
 });
